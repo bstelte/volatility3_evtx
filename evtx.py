1bf6acd35a061816cd95238eda43a259fef1ab3a
Björn Stelte <bstelte@users.noreply.github.com>
2022-01-25 13:36:06 +0100

    Update evtx.py
    required framework update

--- evtx.py
+++ evtx.py
@@ -72,7 +72,7 @@ class EvtxLogs(interfaces.plugins.PluginInterface, timeliner.TimeLinerInterface)
     class for extracting EVTX logs
     """
 
-
+    _required_framework_version = (2, 0, 0)
     _version = (1, 0, 0)
 
     @classmethod
